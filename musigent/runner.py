24e88d10ae153635b18639b552fea6778cd5f0d2
MZ <varedat@gmail.com>
2025-11-30 00:04:14 +0300

    Include time info in handle_request response
    Added time information to the response of handle_request.

--- musigent/runner.py
+++ musigent/runner.py
@@ -12,10 +12,16 @@ class MusigentRunner:
         self.planner = PlannerAgent(self.memory)
         self.composer = ComposerAgent(self.memory)
         self.quality = QualityAgent(self.memory)
+        self.time_agent = TimeAgent(self.memory)
 
     def handle_request(self, mode, prompt, duration_sec=30):
         plan = self.planner.plan(mode, prompt, duration_sec)
         draft = self.composer.compose(plan)
         eval_ = self.quality.evaluate(draft)
         self.memory.save_interaction(plan, draft, eval_)
-        return {"plan": plan, "draft": draft, "evaluation": eval_}
+        time_info = self.time_agent.get_time_info()
+        return {"plan": plan,
+                "draft": draft,
+                "evaluation": eval_,
+                "time_info": time_info
+        }
