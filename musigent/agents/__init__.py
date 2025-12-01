2031d9598a0fc1007e1e26fac46aaba22465d341
MZ <varedat@gmail.com>
2025-12-01 14:40:27 +0300

    Add JingleAgent and JingleInput imports

--- musigent/agents/__init__.py
+++ musigent/agents/__init__.py
@@ -2,3 +2,6 @@ from .planner import PlannerAgent
 from .composer import ComposerAgent
 from .quality import QualityAgent
 from .time import get_utc_time
+from .jingle import JingleAgent, JingleInput
+
+
